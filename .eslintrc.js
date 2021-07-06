e14330da804c2a29ff2c1aedb94ecb322ad0bb6a
Renee de Kruijf <renee@webdesignpuntnl.com>
2021-07-06 15:17:56 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "plugins": ["prettier"],
+    "extends": ["airbnb-base", "prettier"],
+    "env": {
+        "es6": true,
+        "browser": true,
+        "node": true,
+        "jquery": true
+    },
+
+    "rules": {
+        "prettier/prettier": ["error"],
+        "linebreak-style": 0, //0 is off, 1 is warn, 2 is error
+        "prefer-arrow-callback": 1,
+        "no-console": 0,
+        "no-param-reassign": 1,
+        "no-return-assign": 0
+    }
+};
