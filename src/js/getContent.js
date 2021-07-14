47864a7f348a8ff5a94b56ddde413c1a727e44d6
Renee de Kruijf <renee@webdesignpuntnl.com>
2021-07-14 15:53:22 +0200

    added print and pdf buttons

--- src/js/getContent.js
+++ src/js/getContent.js
@@ -1,5 +1,11 @@
 const removeClasses = Array.from(document.querySelector('main').getElementsByTagName('*'));
 const lis = document.querySelectorAll('ol li');
+const printButton = document.querySelector('.printButton');
+
+
+function printRapport() {
+  window.print();
+}
 
 removeClasses.forEach(item => {
   item.hasAttribute('class') ? item.removeAttribute('class') : null;
@@ -13,4 +19,6 @@ lis.forEach(li => {
   const html = `<span>${firstSpan}</span> - <a href="${secondSpan}">${secondSpan}</a>`;
   li.innerHTML = '';
   li.innerHTML = html;
-})
+});
+
+printButton.addEventListener('click', printRapport);
